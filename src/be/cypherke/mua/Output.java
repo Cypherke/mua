e00be76b160a0d3063ddf67272ee24383366c648
Jensen Somers <jensen@jsomers.be>
2019-08-18 14:56:10 +0200

    execute teleport as entity (fixes #3)

--- src/be/cypherke/mua/Output.java
+++ src/be/cypherke/mua/Output.java
@@ -19,17 +19,17 @@ public class Output {
     }
 
     public void sendTeleport(String who, Coordinate coordinate) throws IOException {
-        writer.write("tp " + who + " " + coordinate.getX() + " " + coordinate.getY() + " " + coordinate.getZ() + "\n");
+        writer.write("execute as " + who + " run teleport " + who + " " + coordinate.getX() + " " + coordinate.getY() + " " + coordinate.getZ() + "\n");
         writer.flush();
     }
 
     public void sendTeleport(String who, String where) throws IOException {
-        writer.write("tp " + who + " " + where + "\n");
+        writer.write("execute as " + who + " run teleport " + who + " " + where + "\n");
         writer.flush();
     }
 
     public void sendGetCoordinates(String who) throws IOException {
-        writer.write("tp " + who + " ~ ~ ~\n");
+        writer.write("execute as " + who + " run teleport " + who + " ~ ~ ~\n");
         writer.flush();
     }
 
